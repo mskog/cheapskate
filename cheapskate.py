3ea877052402e9198e86a61e8dcb3a8c576f6dbc
Magnus Skog <magnus.m.skog@gmail.com>
2014-04-10 20:45:12 +0200

    Initial

---
+++ cheapskate.py
@@ -0,0 +1,28 @@
+#!venv/bin/python
+from flask import Flask, jsonify, request, abort
+from newspaper import Article
+
+app = Flask(__name__)
+
+@app.route('/details', methods = ['GET'])
+
+def get_details():
+    url = request.args.get('url', '')
+    if not url:
+      abort(400)
+
+    article = Article(url)
+    article.download()
+    article.parse()
+
+    
+    result = {
+      "url": url,
+      "top_image": article.top_image,
+      "text": article.text
+    }
+
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug = True)
