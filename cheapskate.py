26026416f8af04e85c5093cd6c43462bd45c726a
Magnus Skog <magnus.m.skog@gmail.com>
2015-05-05 20:52:07 +0200

    Elegant and idiomatic solution to the image problem

--- cheapskate.py
+++ cheapskate.py
@@ -1,5 +1,5 @@
 #!venv/bin/python
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from newspaper import Article
 
 app = Flask(__name__)
@@ -9,8 +9,8 @@ app = Flask(__name__)
 def get_index():
   return 'Hello'
 
-@app.route('/details', methods = ['GET'])
 
+@app.route('/details', methods = ['GET'])
 def get_details():
     url = request.args.get('url', '')
     if not url:
@@ -36,6 +36,37 @@ def get_details():
     }
 
     return jsonify(result)
+
+@app.route('/image', methods = ['GET'])
+def get_image():
+  url = request.args.get('url', '')
+  if not url:
+    abort(400)
+
+  image_extensions = ['.jpg', '.gif', '.png', '.jpg', '.bmp', '.webp', '.webm']
+  extension = url[url.rfind('.'):]
+
+  if extension in image_extensions:
+    return redirect(url)
+
+  article = Article(url)
+  article.download()
+
+  try:
+    article.parse()
+  except (IOError, UnicodeDecodeError):
+    return '', 422
+
+  try:
+    top_image = article.top_image.rsplit('?',1)[0]
+  except AttributeError:
+    top_image = ''
+
+  if not top_image == '':
+    return redirect(top_image)
+  else:
+    return '', 422
+
 
 if __name__ == '__main__':
     app.run(debug = True)
